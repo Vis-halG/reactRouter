0ec1ec588576b253fbdd739db4809538be76314e
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-01-03 12:32:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default App;
