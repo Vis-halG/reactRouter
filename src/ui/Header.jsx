d39381cb35e8fc123af72e8e42d86fa801cfa1b7
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-01-04 17:50:03 +0530

    Added Navbar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import "./header.css";
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <div>
+        <h2>Logo</h2>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Tech stack</li>
+          <li>Contact</li>
+        </ul>
+        <button>Buy Now</button>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
