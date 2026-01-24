355601a213f25cc2b4bdca2f765b0efcc49350c7
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-01-24 17:46:30 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Services from './components/Services.jsx'
+import Contact from './components/Contact.jsx'  
 function App() {
   return (
     <div>
       <Navbar />
 
-      
+      <Routes>
+        <Route path="/" element= {<Home/>} />
+        <Route path="/about" element= {<About/>} />
+        <Route path="/services" element= {<Services/>} />
+        <Route path="/contact" element= {<Contact/>} />
+      </Routes>
     </div>
   )
 }
